6919bea0e83bc77efcc353a8a75f4c786327cdcd
dikaapriandipratama <dikaapriandipratama82@gmail.com>
2026-09-17 17:58:13 +0700

    Add files via upload
    tugas algoritma

---
+++ main.py
@@ -0,0 +1,9 @@
+sisi = float (input("Masukan panjang sisi persegi:"))
+
+keliling = 4 * sisi
+print(f"keliling persegi adalah:  {keliling}")
+
+# menghitung keliling persegi 
+sisi = 5 
+k = 4 * sisi
+print("maka hasil dari 4 x", sisi, "=" , k)
